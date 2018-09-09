f32b585920e46ff594ee4d955b9da8fbf43264a7
Elvey <matt.elvey@gmail.com>
2018-09-09 15:38:20 +0930

    add share button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,13 +25,17 @@ class Footer extends Component {
 		const className = this.props.className ? `${styles.content} ${this.props.className}` : styles.content;
 
 		let statusBtn = undefined;
+		let middleBtn = (<button><FontAwesomeIcon icon="plus" />Add</button>);
+
 		if(this.props.showComplete) {
 			statusBtn = (<button onClick={this.props.closeDialog}><FontAwesomeIcon icon="times" />Close</button>);
+			middleBtn = (<button><FontAwesomeIcon icon="share-alt" />Share</button>);
 		} else if(this.props.startTime) { 
 			statusBtn = (<button onClick={this.props.stopRide}><FontAwesomeIcon icon="flag-checkered" />Stop</button>);
 		} else {
 			statusBtn = (<button onClick={this.props.startRide}><FontAwesomeIcon icon="check" />Start</button>);
 		}
+
 
 		return (
 				<div className={styles.root}>
@@ -40,10 +44,7 @@ class Footer extends Component {
 							<FontAwesomeIcon icon="heart" />
 							Save
 						</button>
-						<button>
-							<FontAwesomeIcon icon="plus" />
-							Add
-						</button>
+						{middleBtn}
 						{statusBtn}
 					</div>
 				</div>
